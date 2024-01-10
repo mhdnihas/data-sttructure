bdba14eeb3f68e0db66457908a457abcc74b3035
mhdnihas <muhammednihas8589@gmail.com>
2024-01-10 11:28:30 +0530

    first commit

---
+++ Leetcode/leetcode1.py
@@ -0,0 +1,9 @@
+#Qno:1689
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        big=0
+        for x in n:
+            if int(x)>big:
+                big=int(x)
+        return big
